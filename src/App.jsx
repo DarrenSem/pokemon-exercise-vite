f0b7b72865b4954fffc5e5f9463aa23564fb2cbf
Darren Semotiuk <darrensemotiuk@gmail.com>
2023-05-26 12:09:00 -0600

    Create <PokedexTable /> to render  pokemonArray

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,15 @@
-// import { useState } from "react";
-// import "./App.css";
-
 // TODO: future make this use process.env (see vite.config.js)
 const mode = /\/dist\/?/.test(location.pathname) ? "production" : "development";
 
-const bulbasaur = {
-	id: 1,
-	name: "Bulbasaur",
-	types: ["grass"],
-	// sprite: "https://pokemon.com/pictures/bulbasaur.png",
-	sprite: "001.png",
-};
-
 // reusable <PokemonRow /> component (or the equivalent in your framework)
-function PokemonRow(props) {
-	//  takes in bulbasaur as a property
-	const { bulbasaur } = props;
+function PokemonRow({ pokemon }) {
 	// renders a row with the name, id, type and sprite image
-	const { name, id, types } = bulbasaur;
+	const { name, id, types } = pokemon;
 	const sprite = `${mode === "production" ? "./" : "../"}assets/${
-		bulbasaur.sprite
+		pokemon.sprite
 	}`;
 	return (
-		<table border="1">
+		<tr>
 			<td>name: {name}</td>
 			<td>id: {id}</td>
 			<td>type: {types}</td>
@@ -32,15 +19,46 @@ function PokemonRow(props) {
 					width={"50%"}
 				/>
 			</td>
+		</tr>
+	);
+}
+
+function PokedexTable({ pokedex }) {
+	// for each pokemon render PokemonRow(pokemon)
+	return (
+		<table border="1">
+			{/* <thead></thead> */}
+			<tbody>
+				{pokedex.map(pokemon => (
+					<PokemonRow
+						key={pokemon.id}
+						pokemon={pokemon}
+					/>
+				))}
+			</tbody>
 		</table>
 	);
 }
 
+const pokemonArray = [
+	{
+		id: 1,
+		name: "Bulbasaur",
+		types: ["grass"],
+		// sprite: "https://pokemon.com/pictures/bulbasaur.png",
+		sprite: "001.png",
+	},
+	//  {
+	//   id: 2,
+	//   ...
+	// }, ...
+];
+
 function App() {
-	// TODO: #2 "Part 1" create a reusable component
+	// TODO: #7 "Part 2" create a reusable component
 	// https://t3-tools.notion.site/Pokedex-Problem-90f9dcfff10d4418a6fad44581b1ecff
-	// create a reusable <PokemonRow /> component (or the equivalent in your framework) that takes in bulbasaur as a property and renders a row with the name, id, type and sprite image
-	return <PokemonRow bulbasaur={bulbasaur} />;
+	// create a <PokedexTable /> component that takes in the array and renders all the pokemon in that array.
+	return <PokedexTable pokedex={pokemonArray} />;
 }
 
 export default App;
